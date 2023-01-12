019b32ea7612bffda4fbbec92e92c1dedcec60be
Sasha <slippersasha@gmail.com>
2023-01-12 13:51:00 +0200

    homework2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-"use strict"
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+"use strict";
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 const personalMoovieDB = {
    count: numberOfFilms,
@@ -9,10 +10,22 @@ const personalMoovieDB = {
    privat: false,
 };
 
-let a = +prompt('Один из последних просмотренных фильмов?');
-let b = prompt('На сколько оцените его?');
-let c = +prompt('Один из последних просмотренных фильмов?');
-let d = prompt('На сколько оцените его?');
+for (let i = 0; i < 2; i++){
+   let filmName = prompt('Один из последних просмотренных фильмов?');
+   let yourRating = prompt('На сколько оцените его?');
+   if (filmName.length > 50 || filmName === null || yourRating === null || yourRating.length <= 0 || filmName.length <= 0) {
+      i--;
+      continue;
+   }
+   personalMoovieDB.movies[filmName] = yourRating;
+}
 
-personalMoovieDB.movies[a] = b;
-personalMoovieDB.movies[c] = d;
+if (personalMoovieDB.count < 10 && personalMoovieDB.count > 0) {
+   console.log('Просмотренно довольно мало фильмов');
+}else if (personalMoovieDB.count >= 10 && personalMoovieDB.count <= 30) {
+   console.log('Вы классический зритель');
+}else if(personalMoovieDB.count > 20) {
+   console.log('Вы киноман');
+}else {
+   console.log('Произошла ошибка');
+}
